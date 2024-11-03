27963c9ad9c103041d1924bfea5d763c98d9b3b2
cidi12 <rahmat.tw@outlook.com>
2024-11-03 11:48:32 +0700

    add hover popup

---
+++ public/js/dashboard.js
@@ -0,0 +1,54 @@
+function hoverFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.style.visibility = 'visible';
+}
+const hoverElement = document.getElementById('beranda');
+hoverElement.addEventListener('mouseleave', hidePopupOutside);
+function hidePopupOutside() {
+    var popup = document.getElementById("myPopup");
+    popup.style.visibility = 'hidden';
+}
+
+function hoverFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.style.visibility = 'visible';
+}
+const hoverElement2 = document.getElementById('komunitas');
+hoverElement2.addEventListener('mouseleave', hidePopupOutside2);
+function hidePopupOutside2() {
+    var popup = document.getElementById("myPopup2");
+    popup.style.visibility = 'hidden';
+}
+
+function hoverFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.style.visibility = 'visible';
+}
+const hoverElement3 = document.getElementById('marketplace');
+hoverElement3.addEventListener('mouseleave', hidePopupOutside3);
+function hidePopupOutside3() {
+    var popup = document.getElementById("myPopup3");
+    popup.style.visibility = 'hidden';
+}
+
+function hoverFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.style.visibility = 'visible';
+}
+const hoverElement4 = document.getElementById('pesan');
+hoverElement4.addEventListener('mouseleave', hidePopupOutside4);
+function hidePopupOutside4() {
+    var popup = document.getElementById("myPopup4");
+    popup.style.visibility = 'hidden';
+}
+
+function hoverFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.style.visibility = 'visible';
+}
+const hoverElement5 = document.getElementById('notifikasi');
+hoverElement5.addEventListener('mouseleave', hidePopupOutside5);
+function hidePopupOutside5() {
+    var popup = document.getElementById("myPopup5");
+    popup.style.visibility = 'hidden';
+}
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,51 +1,74 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <title>Dashboard</title>
+    <title>Document</title>
 </head>
 <body>
+    
+</body>
+</html>
+<html lang="en">
 
-    <header>
-        <nav>
-            <div class="left-section">
-                <img src="{{ asset('img/logo.jpg') }}">
-                <div class="searchbar-container">
-                    <label for="searchbar"><i class="fa fa-search"></i></label>
-                    <input type="search" name="" id="searchbar" placeholder="Cari di Fesnuk">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <title>Dashboard</title>
+    </head>
+
+    <body>
+
+        <header>
+            <nav>
+                <div class="left-section">
+                    <img src="{{ asset('img/logo.jpg') }}">
+                    <div class="searchbar-container">
+                        <label for="searchbar"><i class="fa fa-search"></i></label>
+                        <input type="search" name="" id="searchbar" placeholder="Cari di Fesnuk">
+                    </div>
+
                 </div>
-              
-            </div>
-            <div class="mid-section">
-                <i class="fa fa-home fa-2x" aria-hidden="true"><a href=""></a></i>
-                <i class="fa fa-users fa-2x" aria-hidden="true"><a href=""></a></i>
-                <i class="fa fa-shopping-bag fa-2x" aria-hidden="true"><a href=""></a></i>
-                
-            </div>
-            <div class="right-section">
-                <i class="fa fa-comments fa-2x" aria-hidden="true">   <a href=""></a></i>
-                <i class="fa fa-bell fa-2x" aria-hidden="true"> <a href=""></a></i>
-                <img src="{{ asset('img/profile.png') }}">
-                {{-- <form action="logout" method="post">
+                <div class="mid-section">
+                    <i class="fa fa-home fa-2x" id="beranda" onmouseover="hoverFunction()" aria-hidden="true"><a
+                            href=""></a><span class="popuptext" id="myPopup">Beranda</span></i>
+
+                    <i class="fa fa-users fa-2x" id="komunitas" onmouseover="hoverFunction2()" aria-hidden="true"><a
+                        href=""></a><span class="popuptext" id="myPopup2">Komunitas</span></i>
+
+                    <i class="fa fa-shopping-bag fa-2x" id="marketplace" onmouseover="hoverFunction3()" aria-hidden="true"><a
+                        href=""></a><span class="popuptext" id="myPopup3">Marketplace</span></i>
+
+                </div>
+                <div class="right-section">
+                    <i class="fa fa-comments fa-2x" id="pesan" onmouseover="hoverFunction4()" aria-hidden="true"><a
+                        href=""></a><span class="popuptext" id="myPopup4">Pesan</span></i>
+
+                    <i class="fa fa-bell fa-2x" id="notifikasi" onmouseover="hoverFunction5()" aria-hidden="true"><a
+                        href=""></a><span class="popuptext" id="myPopup5">Notifikasi</span></i>
+                    <img src="{{ asset('img/profile.png') }}">
+                    {{-- <form action="logout" method="post">
                     @csrf
                      <button type="submit" >Log out</button>
                 </form> --}}
-            </div>
-           
-        </nav>
-    </header>
-    {{-- <div>
+                </div>
+
+            </nav>
+        </header>
+        {{-- <div>
   Halo {{ Auth::user()->email }}
     </div> --}}
-  
-   
 
-    
 
-   
-</body>
+
+
+     <script src="{{ asset('js/dashboard.js') }}"></script>
+
+    </body>
+
 </html>
