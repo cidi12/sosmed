6b466e27b12e8980af63f92acec5a8f702ccc4fa
cidi12 <rahmat.tw@outlook.com>
2024-11-17 23:34:52 +0700

    fix randomized post

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,15 +13,22 @@ class DashboardController extends Controller
     {
         // $post = Post::inRandomOrder()->get();
         
-        $postOrder = session('post_order', Post::pluck('id')->shuffle()->toArray());
+        // $postOrder = session('post_order', Post::pluck('id')->shuffle()->toArray());
 
-        // Store the order in the session
-        session(['post_order' => $postOrder]);
+        // // Store the order in the session
+        // session(['post_order' => $postOrder]);
     
-        // Fetch posts in the randomized order
-        $post = Post::whereIn('id', $postOrder)
-                     ->orderByRaw("FIELD(id, " . implode(',', $postOrder) . ")")
-                     ->get();
+        // // Fetch posts in the randomized order
+        // $post = Post::whereIn('id', $postOrder)
+        //              ->orderByRaw("FIELD(id, " . implode(',', $postOrder) . ")")
+        //              ->get();
+            // Fetch all post IDs and shuffle them
+    $postOrder = Post::pluck('id')->shuffle()->toArray();
+
+    // Fetch posts in the shuffled order
+    $post = Post::whereIn('id', $postOrder)
+                 ->orderByRaw("FIELD(id, " . implode(',', $postOrder) . ")")
+                 ->get();
     
         return view('dashboard.index', ['posts' => $post]);
     }
