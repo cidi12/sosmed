649004c309d94030a0761e356485729e63ec5b3c
cidi12 <rahmat.tw@outlook.com>
2024-11-25 23:51:28 +0700

    small update profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,12 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    // public function index() {
-    //     $username = Auth::guard('member')->user()->username;
-    //     $email = Auth::guard('member')->user()->email;
-    //     $post = Post::select('id', 'username', 'post_title', 'post_content', 'post_commenter', 'post_comment', 'total_comment', 'likes', 'dislikes', 'shares')->where('email', $email)->orderBy('created_at','desc')->get();
-    //     $currentDateTime = now()->toDateString();
-    //     $likebtn = Interaction::select('post_id', 'commenter', 'likes', 'dislikes', 'shares')->where('commenter', $username)->get();
-    //     return view('dashboard.profile',['posts'=>$post,'likes'=>$likebtn]);
-    // }
+    public function index() {
+        $username = Auth::guard('member')->user()->username;
+        $email = Auth::guard('member')->user()->email;
+        $post = Post::select('id', 'username', 'post_title', 'post_content', 'post_commenter', 'post_comment', 'total_comment', 'likes', 'dislikes', 'shares')->where('email', $email)->orderBy('created_at','desc')->get();
+        $currentDateTime = now()->toDateString();
+        $likebtn = Interaction::select('post_id', 'commenter', 'likes', 'dislikes', 'shares')->where('commenter', $username)->get();
+        return view('dashboard.profile',['posts'=>$post,'likes'=>$likebtn]);
+    }
 }
